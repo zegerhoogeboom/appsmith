b52f977f8bee2203009bd599237ed8d68d7b4020
Nandan Anantharamu <Nandan@thinkify.com>
2020-05-26 22:35:26 +0530

    Removed redundant tests and grouped tests

--- app/client/cypress/integration/Smoke_TestSuite/ApiPaneTests/API_all_sidebar_actions_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/ApiPaneTests/API_all_sidebar_actions_spec.js
@@ -1,11 +1,17 @@
 const testdata = require("../../../fixtures/testdata.json");
 
 describe("API Panel Test Functionality ", function() {
-  it("Test API Name uniqueness fetaure", function() {
+  it("Test API Copy/Move/delete feature", function() {
     cy.log("Login Successful");
     cy.viewport("macbook-15"); //To avoid screen Resize issues
     cy.NavigateToAPI_Panel();
     cy.log("Navigation to API Panel screen successful");
+    cy.CreateAPI("FirstAPI");
+    cy.log("Creation of FirstAPI Action successful");
+    cy.CopyAPIToHome("FirstAPI");
+    cy.DeleteAPI("FirstAPI");
+    cy.MoveAPIToPage();
+    cy.DeleteAPI("FirstAPI");
     cy.CreateAPI("FirstAPI");
     cy.log("Creation of FirstAPI Action successful");
     cy.CreationOfUniqueAPIcheck("FirstAPI");
