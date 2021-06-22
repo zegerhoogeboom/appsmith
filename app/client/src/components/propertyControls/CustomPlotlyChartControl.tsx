eea6c1e7a95448465da14105e9ef069453cc568a
zeger <zeger@equinoxai.com>
2021-06-22 15:36:39 +0200

    Minor fix for custom plotly chart

--- app/client/src/components/propertyControls/CustomPlotlyChartControl.tsx
+++ app/client/src/components/propertyControls/CustomPlotlyChartControl.tsx
@@ -4,20 +4,11 @@ import InputTextControl, { InputText } from "./InputTextControl";
 class CustomPlotlyChartControl extends InputTextControl {
   render() {
     const expected = "{\n  layout: {},\n  data: []\n}";
-    const {
-      propertyValue,
-      isValid,
-      label,
-      placeholderText,
-      dataTreePath,
-      validationMessage,
-    } = this.props;
+    const { dataTreePath, label, placeholderText, propertyValue } = this.props;
     return (
       <InputText
         dataTreePath={dataTreePath}
-        errorMessage={validationMessage}
         expected={expected}
-        isValid={isValid}
         label={label}
         onChange={this.onTextChange}
         placeholder={placeholderText}
